f4df2cf4533f3e4a21e092891a702e6e6e0b7f5f
Chi-O <chioma.o3762@gmail.com>
2022-03-01 21:59:47 -0500

    initial commit; Bedroom and Architect classes

---
+++ src/main/java/CSCI2020U/assignment1/Architect.java
@@ -0,0 +1,10 @@
+package CSCI2020U.assignment1;
+
+import java.awt.*;
+
+// the Architect class represents the director; it is used to make Bedroom objects
+public class Architect {
+    public static void main(String[] args) {
+        Bedroom room = new Bedroom(new Dimension(300, 400), 100, 4, Color.magenta, 3, 1, true, true);
+    }
+}
---
+++ src/main/java/CSCI2020U/assignment1/Bedroom.java
@@ -0,0 +1,31 @@
+package CSCI2020U.assignment1;
+
+import java.awt.Dimension;
+import java.awt.Color;
+
+// the Bedroom class is one of the complex products
+public class Bedroom {
+    // Bedroom attributes
+    private Dimension dimensions; // Dimension object contains width and height
+    private int ceilingHeight;
+    private int floorNumber;
+    private Color wallColor;
+    private int numberOfWindows;
+    private int numberOfDoors;
+    private boolean isDouble;
+    private boolean hasEnsuite;
+
+    // original lengthy constructor
+    public Bedroom(
+            Dimension dimensions, int ceilingHeight, int floorNumber, Color wallColor, int numberOfWindows, int numberOfDoors, boolean isDouble, boolean hasEnsuite
+    ) {
+        this.dimensions = dimensions;
+        this.ceilingHeight = ceilingHeight;
+        this.floorNumber = floorNumber;
+        this.wallColor = wallColor;
+        this.numberOfWindows = numberOfWindows;
+        this.numberOfDoors = numberOfDoors;
+        this.isDouble = isDouble;
+        this.hasEnsuite = hasEnsuite;
+    }
+}
