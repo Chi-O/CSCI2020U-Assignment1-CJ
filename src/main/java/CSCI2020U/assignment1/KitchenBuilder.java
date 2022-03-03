b29982f1d5fd7cd747178f3e9586712587c50de4
Chi-O <chioma.o3762@gmail.com>
2022-03-02 21:56:06 -0500

    finished KitchenBuilder

--- src/main/java/CSCI2020U/assignment1/KitchenBuilder.java
+++ src/main/java/CSCI2020U/assignment1/KitchenBuilder.java
@@ -14,4 +14,59 @@ public class KitchenBuilder implements Builder{
     private int numberOfDoors;
     private boolean hasDishwasher;
     private boolean hasMicrowave;
+
+    public KitchenBuilder() {
+    }
+
+    // implement and override setter methods from Builder interface
+    @Override
+    public KitchenBuilder setDimensions(Dimension dimensions) {
+        this.dimensions = dimensions;
+        return this;
+    }
+
+    @Override
+    public KitchenBuilder setCeilingHeight(int ceilingHeight) {
+        this.ceilingHeight = ceilingHeight;
+        return this;
+    }
+
+    @Override
+    public KitchenBuilder setFloorNumber(int floorNumber) {
+        this.floorNumber = floorNumber;
+        return this;
+    }
+
+    @Override
+    public KitchenBuilder setWallColor(Color wallColor) {
+        this.wallColor = wallColor;
+        return this;
+    }
+
+    @Override
+    public KitchenBuilder setNumberOfWindows(int numberOfWindows) {
+        this.numberOfWindows = numberOfWindows;
+        return this;
+    }
+
+    @Override
+    public KitchenBuilder setNumberOfDoors(int numberOfDoors) {
+        this.numberOfDoors = numberOfDoors;
+        return this;
+    }
+
+    // implement Kitchen-specific methods
+    public KitchenBuilder setDishWasher(boolean hasDishwasher) {
+        this.hasDishwasher = hasDishwasher;
+        return this;
+    }
+
+    public KitchenBuilder setMicrowave(boolean hasMicrowave) {
+        this.hasMicrowave = hasMicrowave;
+        return this;
+    }
+
+    public Kitchen createKitchen() {
+        return new Kitchen(dimensions, ceilingHeight, floorNumber, wallColor, numberOfWindows, numberOfDoors, hasDishwasher, hasMicrowave);
+    }
 }
